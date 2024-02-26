f115ed75a5db9f8eb8150c31b61bc30a4fa6d7b1
Kumar <kumarteja100@gmail.com>
2024-02-26 08:07:23 +0530

    Adding bootstrap and modifying the application UI

---
+++ sepoy-2/main_app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template
+
+app = Flask(__name__,template_folder='templates')
+
+posts = [
+    {
+        'author': 'Kumar',
+        'title': 'Creating your first flask application',
+        'content': 'Follow the steps here : https://github.com/SolKumar/Project-Genesis/tree/main/volunteer',
+        'date_posted': '25 Feb, 2024'
+    },
+    {
+        'author': 'Teja',
+        'title': 'Adding HTML templates to your first flask application',
+        'content': 'Follow the steps here : https://github.com/SolKumar/Project-Genesis/tree/main/sepoy',
+        'date_posted': '26 Feb, 2024'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home",methods=["GET","POST"])
+def home_page():
+    return render_template("home.html",posts=posts,title="Home")
+
+@app.route("/about",methods=["GET","POST"])
+def about_page():
+    return render_template("about.html",title="About")
+
+                 
+if __name__ == "__main__":
+    app.run(debug=True) # Set to True for debugging
+
+
---
+++ sepoy-2/readme.md
@@ -0,0 +1,3 @@
+This is your Third application.
+This is a more advanced app that uses bootstrap for UI. 
+Templates taken from Corey Schafer - https://github.com/CoreyMSchafer/code_snippets/tree/master/Python/Flask_Blog/02-Templates
